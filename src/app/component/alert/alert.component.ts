9648e9adb7dcbb34a00dac84c93cd6f8212162e6
Justtife <farinubolu@gmail.com>
2023-07-06 19:57:17 +0200

    Updated the alert component

--- src/app/component/alert/alert.component.ts
+++ src/app/component/alert/alert.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
 })
-export class AlertComponent {
+export class AlertComponent implements OnInit {
   @Input() response: any;
   showAlert: boolean = true;
+  ngOnInit(): void {
+    this.clearAlert();
+  }
   onCancelClick(): void {
     this.showAlert = false;
   }
-  resetAlert(): void {
-    this.showAlert = true;
+  clearAlert() {
     setTimeout(() => {
       this.showAlert = false;
     }, 5000);
